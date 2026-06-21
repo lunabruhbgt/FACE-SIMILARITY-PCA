add2c5bb66a0ef9da14fbdebc502fd2ff5742468
lunabruhbgt <lunaamilia400@gmail.com>
2026-06-21 19:25:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -607,28 +607,24 @@ anggota = [
         "nama": "Gea Destadia Al-Zahra",
         "ig": "@gea_destadia_10",
         "telp": "0831-5068-7481",
-        "foto": "assets/gea.jpg"
     },
     {
         "inisial": "LA",
         "nama": "Luna Amilia",
         "ig": "@luunaaamiiii",
-        "telp": "0895-3780-96802",
-        "foto": "assets/luna.jpg"
+        "email": "lunaamilia0@gmail.com",
     },
     {
         "inisial": "NA",
         "nama": "Nadia Azizah",
         "ig": "@ndyyzh",
         "telp": "0858-4631-3309",
-        "foto": "assets/nadia.jpg"
     },
     {
         "inisial": "DAAD",
         "nama": "Dalilah Arifah Ariandi DJR",
         "ig": "@adellianav",
         "telp": "0813-1211-6787",
-        "foto": "assets/dalilah.jpg"
     },
 ]
 
